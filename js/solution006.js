4966dc66a9ff6d7edc419dde1327ae81eefa0400
Sebastian Fernandez Serra <serra3882@gmail.com>
2022-05-15 18:39:16 -0300

    solution006 codewars, take int array, take out highest and lowest values, return the sum of the rest of the array

--- js/solution006.js
+++ js/solution006.js
@@ -1,20 +1,30 @@
-/* Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+/* Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
 
-Examples:
-Input: 42145 Output: 54421
+The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
 
-Input: 145263 Output: 654321
+Mind the input validation.
 
-Input: 123456789 Output: 987654321
-
+Example
+{ 6, 2, 1, 8, 10 } => 16
+{ 1, 1, 11, 2, 3 } => 6
+Input validation
+If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
  */
 
-let n = 42145
 
-function descendingOrder(n){
-    
-    let nArray = n.toString().split('')
-    return Number(nArray.sort((a, b) => b - a, 0).join(''))
 
+let numbers = [5,3,2,2,4,0.5]
+
+function sumArray(array) {
+  if (array == null || array.length == 1) {return 0} //imput validation
+  else {
+    array.sort((a, b) => a - b) //order from less to more
+    array.pop()
+    array.shift()
+    return array.reduce((a, b) => a + b, 0) //after taking highest and lowest values, sum up and return the result
+   
   }
-  descendingOrder(n)
+  
+}
+
+console.log(sumArray(numbers))
