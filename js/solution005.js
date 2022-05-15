b22ffad1ace4826dd4752adcb15bbb20b0ed6947
Sebastian Fernandez Serra <serra3882@gmail.com>
2022-05-15 14:26:31 -0300

    solution005, return a copy of a int  array minus its lowest value without altering the order of the elements nor mutating the original array. there is probably a more elegant solution to this one out there haha

--- js/solution005.js
+++ js/solution005.js
@@ -1,20 +1,36 @@
-/* Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+/* The museum of incredible dull things
+The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
 
-Examples:
-Input: 42145 Output: 54421
+However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
 
-Input: 145263 Output: 654321
+Task
+Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
 
-Input: 123456789 Output: 987654321
+Don't change the order of the elements that are left.
 
- */
+Examples
+* Input: [1,2,3,4,5], output= [2,3,4,5]
+* Input: [5,3,2,1,4], output = [5,3,2,4]
+* Input: [2,2,1,2,1], output = [2,2,2,1] */
 
-let n = 42145
+let numbers = [5,3,2,1,4,0.5]
 
-function descendingOrder(n){
-    
-    let nArray = n.toString().split('')
-    return Number(nArray.sort((a, b) => b - a, 0).join(''))
-
+function removeSmallest(numbers){
+  
+  let lowerValue; //we make a variable that will contain the lowest number of the array
+  for(i = 0; i < numbers.length; i++){
+    if (i === 0) {
+      lowerValue = numbers[0]
+    }; //we give it the starting value of the first number of the array in a way that doesnt rewrites it every loop. 
+    if (lowerValue > numbers[i]){
+      lowerValue = numbers[i]
+    }; //if the current value in lowerValue is bigger than the one we are looping on, we transfer numbers[i] to lowerValue
   }
-  descendingOrder(n)
+  let numbers2 = numbers.map(n => n) //we obtain a copy of the numbers array so we dont mutate the original
+  let lowerValueIndex = numbers2.indexOf(lowerValue) //we obtain the index of the lowest value. if lower value is duplicated, we get the first one.
+  numbers2.splice(lowerValueIndex, 1) //we take out only the lowest value
+  return numbers2
+  }
+
+  console.log(removeSmallest(numbers))
+  
