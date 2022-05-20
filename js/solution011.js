da399519c8c3f52394a0a89f80658c0ed827c251
Sebastian Fernandez Serra <serra3882@gmail.com>
2022-05-20 12:29:29 -0300

    solution 11

--- js/solution011.js
+++ js/solution011.js
@@ -10,13 +10,13 @@ Examples(Operator, value1, value2) --> output
 ('/', 49, 7) --> 7 */
 
 function basicOp(operation, value1, value2){
-    if (operation === '+') {
-      return value1 + value2
-  } else if (operation === '-') {
-      return value1 - value2
-  } else if (operation === '*') {
-      return value1 * value2
-  } else if(operation === '/') {
-      return value1 / value2  
-  }
+  if (operation === '+') {
+    return value1 + value2
+} else if (operation === '-') {
+    return value1 - value2
+} else if (operation === '*') {
+    return value1 * value2
+} else if(operation === '/') {
+    return value1 / value2  
+}
 }
