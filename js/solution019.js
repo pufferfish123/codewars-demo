108bb5215c12a2753e808472c8475180f63c00c4
Sebastian Fernandez Serra <serra3882@gmail.com>
2022-06-05 12:05:29 -0300

    takes speed in km per hour and returns it in cm per second, rounded down to the integer (= floored)

--- js/solution019.js
+++ js/solution019.js
@@ -9,7 +9,7 @@ Note! The input is a Real number (actual type is language dependent) and is >= 0
 function cockroachSpeed(s) {
   //first, we multiply the km x 100000 to get cm per hour
   //second, we divide the number by 3600 to obtan cm per second
-  let cmPerHour = s * 1000
+  let cmPerHour = s * 100000
   return (Math.floor(cmPerHour/3600))
 }
 
