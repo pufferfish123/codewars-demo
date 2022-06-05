8b2ce9b7c59d6a97f268a091ff6b6e5508da8b6d
Sebastian Fernandez Serra <serra3882@gmail.com>
2022-06-05 11:58:29 -0300

    added sudo code

--- js/solution019.js
+++ js/solution019.js
@@ -6,5 +6,10 @@ For example:
 Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
  */
 
-
+function cockroachSpeed(s) {
+  //first, we multiply the km x 100000 to get cm per hour
+  //second, we divide the number by 3600 to obtan cm per second
+  let cmPerHour = s * 1000
+  return (Math.floor(cmPerHour/3600))
+}
 
